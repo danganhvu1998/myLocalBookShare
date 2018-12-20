978749096c0ed3d1cff54b963c094b856695b961
Dang Anh Vu <danganhvu1998@gmail.com>
2018-12-20 23:45:28 +0900

    add Book, Comment model

---
+++ localBookShare/database/migrations/2018_12_20_141928_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('author')->nullable();
+            $table->string('quality')->nullable();
+            $table->string('intro')->nullable();
+            $table->string('image')->nullable();
+            $table->string('language')->default('English');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
