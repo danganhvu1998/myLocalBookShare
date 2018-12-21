39bfebd459644bcbd4ca2b3c2bd1b9e037e2e3f9
Dang Anh Vu <danganhvu1998@gmail.com>
2018-12-21 09:31:54 +0900

    add checkAdmin middleware

--- localBookShare/routes/web.php
+++ localBookShare/routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+### Users Route ###
+
+//************************ ### ************************/\\
+
+### Admin Route ###
