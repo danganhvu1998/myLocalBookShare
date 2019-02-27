fde3b30784d1ebf274d1ad8900ccace87dd1ffe8
Dang Anh Vu <danganhvu1998@gmail.com>
2019-02-27 20:16:06 +0900

    this is better I guess :(((

--- localBookShare/resources/views/books/allBooks.blade.php
+++ localBookShare/resources/views/books/allBooks.blade.php
@@ -3,16 +3,18 @@
 @section('content')
     <div class="row">
         @foreach ($books as $book)
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="row">
-                    <div class="col-md-5" >
+                    <div class="col-md-4" >
                         <img src="{{$book->image}}" alt="Book's Image" width="100%">
                     </div>
-                    <div class="col-md-7">
+                    <div class="col-md-8">
                         <a href="/detail_book/{{$book->id}}"> 
                             <h4><b class="text-info">{{$book->name}}</b></h4>
                         </a>
                         {{__("messages.author")}}: <b>{{$book->author}}</b><br>
+                        <hr>
+                        {{__("messages.quality")}}: <b>{{$book->quality}}</b><br>
                         {{__("messages.language")}}: <b>{{$book->language}}</b><br>
                         <hr>
                         @guest
@@ -31,7 +33,7 @@
                     </div>
                 </div>
             </div>
-            @if ($loop->index%3==2)
+            @if ($loop->index%2==1)
                 </div><hr><div class="row">
             @endif
         @endforeach
